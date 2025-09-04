d6d0c515899567f936675eaae0aaf43b5220cee6
Janmejoy Mahato <janmejoymahato529@gmail.com>
2025-09-05 03:55:33 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const db = require("./db");
 
 const app = express();
