4534ae9242fb8d8488943d3a81a2776a2d368deb
Janmejoy Mahato <janmejoymahato529@gmail.com>
2025-11-08 13:24:09 +0530

    try to figure

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
-const dbPath = path.resolve(__dirname, "voting.db");
+const dbPath = path.resolve(__dirname, "voting2.db");
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) console.error("❌ DB open error:", err.message);
   else console.log("✅ Connected to SQLite database");
